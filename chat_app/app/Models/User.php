dabef33e0350ade035b5852973f63946c8a84af8
djukic03 <djukic.nemanja003@gmail.com>
2025-09-19 18:20:50 +0200

    Migrations, Models and Seed data

--- chat_app/app/Models/User.php
+++ chat_app/app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_blocked',
     ];
 
     /**
@@ -41,7 +43,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- chat_app/database/seeders/DatabaseSeeder.php
+++ chat_app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Message;
+use App\Models\Conversation;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,15 +17,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => "John Doe",
+            'email' => "john@example.com",
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+            'is_blocked' => false
+        ]);
 
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
-                'name' => 'Test User',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
+        User::factory()->create([
+            'name' => "Jane Doe",
+            'email' => "jane@example.com",
+            'password' => bcrypt('password'),
+            'is_admin' => false,
+            'is_blocked' => false
+        ]);
+
+        User::factory(10)->create();
+
+        Message::factory(100)->create();
+        $messages = Message::all();
+
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
+        })->map(function ($groupedMessages) {
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon(),
+            ];
+        })->values();
+
+        Conversation::insertOrIgnore($conversations->toArray());
     }
 }
